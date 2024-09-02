6679cbb7a87870d06812fa7cc15412ddcb3d3e2c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 15:01:15 -0300

    primeira parte do projeto

---
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+
+
+
+//Função principal do projeto (o ponto de entrada, a primeira dos
+// arquivos para o computador conseguir entender a linguagem)
+fun main() {
+
+
+       //1-Código
+    //"println" faz a print de algo
+    println("Hello World!")
+
+    //Variáveis = var, serve para armazenar os dados desejados, dados que variam/modificam,
+    // para o computador conseguir identificar qual variável vai ser pega é necessário um
+    // nome para informar a variável desejada
+    // ex: var magruguinha = "Armeq", ou seja toda vez que eu querer utilizar o nome
+    // "Armeq" basta eu chamar a variavel madruguinha
+
+    var variavel = "Pedro"
+
+    //Aqui eu troquei o valor da variavel para "Paulo" ao invés de "Pedro"
+    variavel = "Paulo"
+    //ao fazer isto a variavel se tornou uma variavel mutavel, uma variavel que
+    // muda seu estado.
+
+    //IMPORTANTE!!!!!! NÃO DÁ PRA TER UMA VÁRIAVEL COM O MESMO NOME!!!
+    println(variavel)
+
+    // Val = esta variável não é possível ser mudada ou seja, o valor de val não da
+    // pra alterar.
+    val nome = "Sabrina"
+
+
+
+
+}
